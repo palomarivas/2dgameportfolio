106f882b3e9859f39fc616db636722d936244cf9
salamiemperor <palomarivas96@gmail.com>
2024-05-02 11:17:37 -0300

    prueba

---
+++ src/utils.js
@@ -0,0 +1,6 @@
+export function displayDialogue(text, onDisplayEnd) {
+    const dialogueUI = document.getElementById("textbox-container");
+    const dialogue = document.getElementById("dialogue");
+
+    dialogueUI.style.display = "block";
+}
