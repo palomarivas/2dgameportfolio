e0f291505e7647872d8bb3ae3ad83f553e5e5ba9
palomarivassms <privas@sms-sudamerica.com>
2024-05-07 16:57:11 -0300

    finished the code and changed the sprites for the player

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,17 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days. I've made a couple projects in that language using Angular and Vite. Anyway regardless of the method, I just like programming.
+    Here is my <a href="https://github.com/palomarivas" target="_blank">Github</a>!`,
+  "cs-degree": `These are my programming certificates. I hung them on the wall because I'm proud of them.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube and anime.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@freecodecamp" target="_blank">freeCodeCamp</a>, in this channel I learned how to do this portfolio actually! I'm learning a lot since I found them <3.`,
+  bed: `This is where I sleep. Great ideas comes when I'm lying on my bed.`,
+  resume: `This is my desk and on it is my resume. <a href="https://www.linkedin.com/in/palomarivas/" target="_blank">Check it out!</a>
+  Contact me at palomarivas96@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of mangas on my shelves. I really like East Asian culture, I've actually studied Korean for 2 years and Japanese for 1 year. I can also speak French, I've studied it throughout all my years in school, and Spanish is actually my native language`,
+  exit: `If you want to exit Paloma's portfolio, just close the tab.`,
+};
